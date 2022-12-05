85f994e5b97426bfa6f5c683f1beaef1f4b4522b
devteni <fatunmbiteniola001@gmail.com>
2022-12-05 16:41:20 +0100

    :rocket: ready to deploy

---
+++ script.js
@@ -0,0 +1,14 @@
+var navBar = document.getElementById("nav-items");
+
+function navToggle() {
+    navBar.classList.toggle('hidden');
+}
+
+// SHOW NAVBAR ON DESKTOP SCREEN
+window.addEventListener("resize", function() {
+    if(window.innerWidth <= 900) {
+        navBar.classList.add('hidden');
+    } else {
+        navBar.classList.remove('hidden');
+    }
+})
